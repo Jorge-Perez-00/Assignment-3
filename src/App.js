4dec1ea52b696614e5de2f191433a0fe9cf8f974
Jorge Perez <jorge.perez59@myhunter.cuny.edu>
2022-04-19 17:46:41 -0400

    Implemented the Customization button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import './App.css';
 import { Component } from 'react'
+//import { Helmet } from 'react-helmet';
 import Clock from './components/Clock'
+import Customize from './components/Customize'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: "Guest" };
+    this.state = { 
+      username: "Guest",
+      backgroundColor: "",
+      textColor: ""
+    };
+
+  }
+
+  updateProfile = (name, bColor, tColor) => {
+    this.setState({
+      username: name,
+      backgroundColor: bColor,
+      textColor: tColor 
+    })
+
+    
+
   }
 
 
-
-
   render() {
+
+    let backgroundColor = this.state.backgroundColor
+    
+    document.body.style.backgroundColor = backgroundColor
+
+    const {username} = this.state;
+    const tColor = this.state.textColor;
+
     return (
-      <div>
-        <h1>Hello, {this.state.username}!</h1>
+      <div style={{color: tColor}}>
+        <h1>Hello, {username}!</h1>
+
         <Clock />
+        <Customize update={this.updateProfile} />
+
       </div>
       
     )
---
+++ src/components/Customize.js
@@ -0,0 +1,86 @@
+import { Component } from 'react'
+
+
+
+class Customize extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            backgroundColor: "",
+            textColor: "",
+            buttonPressed: false
+        }
+
+    }
+
+    handleChange = (event) => {
+        let target = event.target.name;
+        this.setState({ 
+            [target]: event.target.value 
+           
+        });
+    }
+
+    handleClick = () => {
+        this.setState({
+            buttonPressed: true
+        })
+    }
+
+    handleSubmit = (event) => {
+        const name = this.state.name;
+        const backgroundColor = this.state.backgroundColor;
+        const textColor = this.state.textColor;
+
+        this.setState({
+            buttonPressed: false
+        })
+        this.props.update(name, backgroundColor, textColor)
+        event.preventDefault();
+    }
+
+
+    render(){
+
+        const isbuttonClicked = this.state.buttonPressed;
+        let element;
+
+        if(isbuttonClicked){
+            element = (
+                <div>
+                    
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name:
+                            <input name={"name"} type="text" value={this.state.name} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Background Color:
+                            <input name={"backgroundColor"} type="text" value={this.state.backgroundColor} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Text Color:
+                            <input name={"textColor"} type="text" value={this.state.textColor} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="submit" />
+                    </form>
+                </div>)
+        }
+        else{
+            element = <button onClick={this.handleClick}>Customize Profile</button>
+        }
+
+        
+
+        return (
+            <div>
+                {element}
+            </div>
+        )
+    }
+
+}
+
+
+export default (Customize)
